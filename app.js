82e32f0d49649688cb7e00d20c20414c87c22d90
TeeMatex <55246224+TeeMatex@users.noreply.github.com>
2020-11-24 10:45:51 +0100

    xx

--- app.js
+++ app.js
@@ -24,7 +24,9 @@ document.addEventListener("DOMContentLoaded", function (_e) {
         }
     }
     
-    
+    if('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('sw.js');
+};
     /******************************************************************
                             Gestion des événements 
     ******************************************************************/
@@ -280,9 +282,7 @@ document.addEventListener("DOMContentLoaded", function (_e) {
     // Ensemble de lignes 
     var lignes = {};
 
-    if('serviceWorker' in navigator) {
-    navigator.serviceWorker.register('sw.js');
-};
+    
     /******************************************************************
                         Fonctions utiles 
     ******************************************************************/
